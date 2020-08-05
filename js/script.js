3f392b0e5b17aca38c48acd5aa343e1955b87df9
cgsaleh83 <cgsaleh83@gmail.com>
2020-08-05 16:24:55 +0600

    new

--- js/script.js
+++ js/script.js
@@ -3,30 +3,29 @@
 
 //gellary item
 
-const filterButtons=document.querySelector("#filter-btns").children;
-const items=document.querySelector(".protfolio-gellery").children;
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".protfolio-gellery").children;
 
 
- for(let i=0; i<filterButtons.length; i++){
- 	filterButtons[i].addEventListener("click",function(){
- 		for(let j=0; j<filterButtons.length; j++){
- 			filterButtons[j].classList.remove("active")
- 		}
- 		this.classList.add("active");
- 		const target=this.getAttribute("data-target")
+for (let i = 0; i < filterButtons.length; i++) {
+	filterButtons[i].addEventListener("click", function () {
+		for (let j = 0; j < filterButtons.length; j++) {
+			filterButtons[j].classList.remove("active")
+		}
+		this.classList.add("active");
+		const target = this.getAttribute("data-target")
 
- 		for(let k=0; k<items.length; k++){
- 			items[k].style.display="none";
- 			if(target==items[k].getAttribute("data-id"))
- 			{
- 				items[k].style.display="block";
- 			}
- 			if(target=="all"){
- 				items[k].style.display="block";
- 			}
- 		}
- 	})
- } 
+		for (let k = 0; k < items.length; k++) {
+			items[k].style.display = "none";
+			if (target == items[k].getAttribute("data-id")) {
+				items[k].style.display = "block";
+			}
+			if (target == "all") {
+				items[k].style.display = "block";
+			}
+		}
+	})
+} 
  
  //light box item
 
@@ -187,9 +186,9 @@ const navbar = document.querySelector(".navbar");
 const hamBurger=document.querySelector(".ham-burger");
 
 
-hamBurger.addEventListener("click",function(){
-	document.querySelector(".navbar").classList.toggle("show");
-})
+// hamBurger.addEventListener("click",function(){
+// 	document.querySelector(".navbar").classList.toggle("show");
+// })
 
 
 
@@ -198,10 +197,10 @@ $(document).mousemove(function (e) {
 	
 })
 
-AOS.init({
-	offset: 400,
-	duration: 1000
-});
+	AOS.init({
+		offset: 400,
+		duration: 1000
+	});
 
 
 	//------------------------ bubble section
